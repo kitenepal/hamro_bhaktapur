a2b9b48ef458d5007dd2211ea714ddb99467a35e
Pirates357 <hacking.pirates@gmail.com>
2018-03-09 14:59:05 +0545

    topbar, navbar, hero, product sidebar

---
+++ js/hero.js
@@ -0,0 +1,7 @@
+$(function() {
+    $("<link/>", {
+        rel: "stylesheet",
+        type: "text/css",
+        href: "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,800,700,600,300"
+    }).appendTo("head");
+});
